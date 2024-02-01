c52d3dce6df1761e4ac1c83a03afcd129b33d8a1
Sebastian <139753003+Iakobson@users.noreply.github.com>
2024-02-01 12:01:46 +0200

    Delete src directory

--- src/services/get-data.ts
+++
@@ -1,6 +0,0 @@
-// @/services/get-data.ts
-import { unstable_noStore as noStore } from 'next/cache';
-// function allows you to make queries to the database
-import { sql } from '@vercel/postgres';
-
-/* -----  ----- */
