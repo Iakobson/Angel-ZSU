daec9e636c1729a710e40930ca6118b75dd51cb2
Sebastian <139753003+Iakobson@users.noreply.github.com>
2024-02-19 12:44:15 +0200

    Update FundTeam.tsx

--- src/views/Home/FundTeam.tsx
+++ src/views/Home/FundTeam.tsx
@@ -7,15 +7,14 @@ import useMediaQuery from '@mui/material/useMediaQuery';
 // MUI components
 import Box from '@mui/material/Box';
 import Container from '@mui/material/Container';
-import Typography from '@mui/material/Typography';
 import IconButton from '@mui/material/IconButton';
 // MUI icons
 import IconArrowBack from '@mui/icons-material/ArrowBack';
 import IconArrowForward from '@mui/icons-material/ArrowForward';
 // views components
-import SectionHeader from '@/views/Common/SectionHeader';
-import TeamMember from '@/views/Home/team/TeamMember';
-import СandidateButton from '@/views/Home/team/СandidateButton';
+import SectionHeader from '@/views/CommonUI/SectionHeader';
+import TeamMember from '@/views/Home/teamUI/TeamMember';
+import СandidateButton from '@/views/Home/teamUI/СandidateButton';
 // info from the database
 import { ourMembersData } from '@/services/fund-common-data';
 
@@ -54,13 +53,15 @@ const StyledDots = styled('ul')(({ theme }) => ({
 
 const FundTeam = () => {
   const { breakpoints } = useTheme();
-  const matchMobileView = useMediaQuery(breakpoints.down('md'));
+  const matchMobileView = useMediaQuery(breakpoints.down('sm'));
+  const matchTabletView = useMediaQuery(breakpoints.down('md'));
 
   const sliderConfig:Settings = {
-    infinite: true, speed: 300, // autoplay: true,
-    slidesToShow: matchMobileView ? 1 : 3, slidesToScroll: 1,
+    // infinite: true, speed: 300, autoplay: true,
+    slidesToShow: matchMobileView ? 1 : (matchTabletView ? 2 : 3), 
+	slidesToScroll: 1,
     prevArrow: <SliderArrow type="prev" />, nextArrow: <SliderArrow type="next" />,
-    dots: !matchMobileView, appendDots: (dots) => <StyledDots>{dots}</StyledDots>,
+    dots: !matchMobileView & !matchTabletView, appendDots: (dots) => <StyledDots>{dots}</StyledDots>,
     customPaging: () => (
       <Box sx={{ height:10, width:30, backgroundColor:'divider', display:'inline-block', borderRadius:4 }}/>
     ),
