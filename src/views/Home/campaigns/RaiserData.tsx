7901fc4194ef400d95106e67ee77838241adc962
Sebastian <139753003+Iakobson@users.noreply.github.com>
2024-02-20 14:57:30 +0200

    Update RaiserData.tsx

--- src/views/Home/campaigns/RaiserData.tsx
+++ src/views/Home/campaigns/RaiserData.tsx
@@ -1,11 +1,11 @@
 // @/views/Home/campaignsUI/RaiserData.tsx
 import CollectCard from '@/views/Home/campaignsUI/CollectCard';
 
+import {IFundRaising} from '@/models/interfaces';
 // get info from data base
 import { fetchAllFundRaiserData } from '@/services/get-data';
 
-
-export default async function FundRaiserData() {
+export default async function RaiserData() {
   const fundRaisers = await fetchAllFundRaiserData();
 
   if (!fundRaisers || fundRaisers.length === 0) {
