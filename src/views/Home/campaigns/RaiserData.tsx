cbd597a12b3a7f0d412db4f59208bdfe93ba552d
Sebastian <139753003+Iakobson@users.noreply.github.com>
2024-02-20 17:25:00 +0200

    Update RaiserData.tsx

--- src/views/Home/campaigns/RaiserData.tsx
+++ src/views/Home/campaigns/RaiserData.tsx
@@ -5,20 +5,25 @@ import {IFundRaising} from '@/models/interfaces';
 // get info from data base
 import { fetchAllFundRaiserData } from '@/services/get-data';
 
-export default async function RaiserData() {
-  const fundRaisers = await fetchAllFundRaiserData();
+const RaiserData = async () => {
+  try {
+    const fundRaisers = await fetchAllFundRaiserData();
 
-  if (!fundRaisers || fundRaisers.length === 0) {
-    console.log('No Fund Raisers data received.');
-    return <p>No Fund Raisers available.</p>;
+    if (!fundRaisers || fundRaisers.length === 0) {
+      console.log('No Fund Raisers data received.');
+      return <p>No Fund Raisers info available.</p>;
+    }
+
+    return (
+      <div>
+        {fundRaisers.map((fundRaiser, index) => (
+          <CollectCard key={index} collectData={fundRaiser} />
+        ))}
+      </div>
+    );
+  } catch (error) {
+    console.error('Error fetching fund raiser data:', error);
+    return <p>Error fetching fund raiser data. Please try again later.</p>;
   }
-
-  return (
-    <>
-      {fundRaisers.map((fundRaiser, index) => (
-        <CollectCard key={index} collectData={fundRaiser} />
-      ))}
-
-    </>
-  );
-}
+};
+export default RaiserData;
